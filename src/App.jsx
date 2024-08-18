82da59a8d743a89474f85c92a057ba03f77bd3b5
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-08-18 09:59:39 +0700

    cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import axios from "../util/api.customize";
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <>
-      <p>Hello World</p>
-    </>
-  );
+  useEffect(() => {
+    callAPI();
+  }, []);
+  const callAPI = async () => {
+    const res = await axios("/v1/api/");
+    console.log(res);
+  };
+  return <></>;
 }
 
 export default App;
