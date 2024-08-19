7d763c084ef47857de76d837d5262d8c682bda6e
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-08-19 10:49:53 +0700

    FE verify

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Space, Table, Tag } from "antd";
+import { Descriptions, message, notification, Space, Table, Tag } from "antd";
 import { getAllUser } from "../../util/api";
 const UserPage = () => {
   const [dataUser, setDataUser] = useState([]);
   useEffect(() => {
     const getUser = async (req, res) => {
       const data = await getAllUser();
-      console.log(data);
+      console.log(data.message);
       if (data.data) {
         setDataUser(data.data);
+      } else {
+        notification.error({
+          message: "Error 401",
+          descriptions: data.message ?? "Error",
+        });
       }
     };
     getUser();
